d00db1db44fcf8e02cbcedebae4d8b950bd86b08
Junior Alves <junioralves.webdeveloper@gmail.com>
2022-03-04 12:47:56 -0300

    Create params

--- src/App.js
+++ src/App.js
@@ -3,12 +3,20 @@ import React from "react";
 import './App.css';
 import Bar from "./lib/components/Bar";
 
+const Items = [
+  {id: 1, caption: 'Novos Clientes', value: '200', percent: '100%'},
+  {id: 2, caption: 'Visita', value: '168', percent: '80.8%'},
+  {id: 3, caption: 'Proposta', value: '168', percent: '50.8%'},
+  {id: 4, caption: 'Garantia', value: '168', percent: '30.8%'},
+  {id: 5, caption: 'Formalização', value: '168', percent: '22.8%'},
+  {id: 5, caption: 'Concluídos', value: '168', percent: '7.58%'},
+];
 const App = () => (
   <div className="container">
     <h1>Meta Charts by Junior Alves</h1>
     <div className='card'>
     <h2 className='card__title'>Funil de conversão</h2>
-      <Bar />
+      <Bar items={Items} />
     </div>
   </div>
 );
--- src/lib/components/Bar/index.js
+++ src/lib/components/Bar/index.js
@@ -2,7 +2,21 @@ import React from 'react';
 
 import './Bar.css';
 
-const Bar = () => (
+  function Bar(props){
+    const columnList = (
+        <ul className='columns__lists'>
+            {props.items.map((item) =>
+                <li key={item.id}>
+                    <span className='columns__lists--caption'>{item.caption}</span>
+                    <span className='columns__lists--bar' style={{height: item.percent}}>
+                        <span className='columns__lists--bar-value'>{item.value}</span>
+                        <span className='column__tooltip'>{item.percent}</span>
+                    </span>
+                </li>
+            )}
+        </ul>
+    )
+    return(
     <div className='lines'>
             <ul className='lines__lists'>
                 <li>
@@ -22,51 +36,8 @@ const Bar = () => (
                     <span className='line'></span>
                 </li>
             </ul>
-            <ul className='columns__lists'>
-                <li>
-                    <span className='columns__lists--caption'>Novos Clientes</span>
-                    <span className='columns__lists--bar' style={{height: '100%'}}>
-                        <span className='columns__lists--bar-value'>200</span>
-                        <span className='column__tooltip'>100%</span>
-                    </span>
-                </li>
-                <li>
-                    <span className='columns__lists--caption'>Visita</span>
-                    <span className='columns__lists--bar' style={{height: '80.8%'}}>
-                        <span className='columns__lists--bar-value'>189</span>
-                        <span className='column__tooltip'>80,8%</span>
-                    </span>
-                </li>
-                <li>
-                    <span className='columns__lists--caption'>Proposta</span>
-                    <span className='columns__lists--bar' style={{height: '50.8%'}}>
-                        <span className='columns__lists--bar-value'>189</span>
-                        <span className='column__tooltip'>50,8%</span>
-                    </span>
-                </li>
-                <li>
-                    <span className='columns__lists--caption'>Garantia</span>
-                    <span className='columns__lists--bar' style={{height: '30.8%'}}>
-                        <span className='columns__lists--bar-value'>189</span>
-                        <span className='column__tooltip'>30,8%</span>
-                    </span>
-                </li>
-                <li>
-                    <span className='columns__lists--caption'>Formalização</span>
-                    <span className='columns__lists--bar' style={{height: '21.8%'}}>
-                        <span className='columns__lists--bar-value'>189</span>
-                        <span className='column__tooltip'>21,8%</span>
-                    </span>
-                </li>
-                <li>
-                    <span className='columns__lists--caption'>Concluídos</span>
-                    <span className='columns__lists--bar' style={{height: '7.58%'}}>
-                        <span className='columns__lists--bar-value'>189</span>
-                        <span className='column__tooltip'>7,58%</span>
-                    </span>
-                </li>
-            </ul>
+            {columnList}
         </div>
-)
-
+    )
+            }
 export default Bar;
